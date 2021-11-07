0766589f6d2168d0bcf6d88e9c030200378da8d9
yrzer <89992153+yrzer@users.noreply.github.com>
2021-11-07 22:08:06 +0100

    index V2

---
+++ index.js
@@ -0,0 +1,172 @@
+            
+            function wyszukiwarka() {
+                var input, filter, ul, li, a, i, txtValue;
+                input = document.getElementById("search");
+                filter = input.value.toUpperCase();
+                ul = document.getElementById("s_lista");
+                li = ul.getElementsByTagName("li");
+                for (i = 0; i < li.length; i++) {
+                    a = li[i].getElementsByTagName("a")[0];
+                    txtValue = a.textContent || a.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        li[i].style.display = "";
+                    } else {
+                        li[i].style.display = "none";
+                    }
+                }
+            }
+            
+            function WinOpen_szczepkowski_1() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/przyklad1a.html", "okienko", "toolbar=no,directories=no,menubar=no,height=1200,width=2000,top=200,left=200");
+            }
+
+            function WinOpen_szczepkowski_1_kod() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/przyklad1a_kod.html", "okienko_1", "toolbar=no,directories=no,menubar=no,height=1200,width=2000,top=200,left=200");
+            }
+			function WinOpen_szczepkowski_2() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zadanie_2.html", "okienko2", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_3() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_3.html", "okienko3", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_4() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_4.html", "okienko4", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_5() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_5.html", "okienko5", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_6() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_6.html", "okienko6", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_7() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_7.html", "okienko7", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_8() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_8.html", "okienko8", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_9() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_9.html", "okienko9", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_8_kod() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_8_kod.html", "okienko01", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_9_kod() {
+                window.open("https://yrzer.github.io/pracownia_witr_i_apl/zad_9_kod.html", "okienko02", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_10_kod() {
+                window.open("szczepkowski-zadanie10_kod.html", "okienko03", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_11_kod() {
+                window.open("szczepkowski-zadanie11_kod.html", "okienko04", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_12_kod() {
+                window.open("szczepkowski-zadanie12_kod.html", "okienko05", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_13_kod() {
+                window.open("szczepkowski-zadanie13_kod.html", "okienko02", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_10() {
+                window.open("szczepkowski-zadanie10.html", "okienko06", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_11() {
+                window.open("szczepkowski-zadanie11.html", "okienko07", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_12() {
+                window.open("szczepkowski-zadanie12.html", "okienko08", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_13() {
+                window.open("szczepkowski-zadanie13.html", "okienko10", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_15() {
+                window.open("zad_15.html", "okienko11", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_16() {
+                window.open("zad_16.html", "okienko12", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_17() {
+                window.open("zad_17.html", "okienko13", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_18() {
+                window.open("zad_18.html", "okienko14", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+			function WinOpen_szczepkowski_15_kod() {
+                window.open("zad_15_kod.html", "okienko15", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_17_kod() {
+                window.open("zad_17_kod.html", "okienko16", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+			function WinOpen_szczepkowski_18_kod() {
+                window.open("zad_18_kod.html", "okienko17", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_19() {
+                window.open("zad_19.html", "okienko18", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_19_kod() {
+                window.open("zad_19_kod.html", "okienko19", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_21() {
+                window.open("zad_21.html", "okienko20", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_21_kod() {
+                window.open("zad_21_kod.html", "okienko21", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_23() {
+                window.open("zad_23.html", "okienko22", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_23_kod() {
+                window.open("zad_23_kod.html", "okienko23", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_23_js_kod() {
+                window.open("zad_23_js_kod.html", "okienko23", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_62() {
+                window.open("zad_62.html", "okienko24", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_63_kod() {
+                window.open("zad_63_kod.html", "okienko26", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_63() {
+                window.open("zad_63.html", "okienko25", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_64_kod() {
+                window.open("zad_64_kod.html", "okienko27", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_64() {
+                window.open("zad_64.html", "okienko28", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_65_kod() {
+                window.open("zad_65_kod.html", "okienko29", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_65() {
+                window.open("zad_65.html", "okienko30", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_66() {
+                window.open("zad_66.html", "okienko32", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_67_kod() {
+                window.open("zad_67_kod.html", "okienko33", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_67() {
+                window.open("zad_67.html", "okienko34", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+
+            function WinOpen_szczepkowski_68_kod() {
+                window.open("zad_68_kod.html", "okienko35", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_68() {
+                window.open("zad_68.html", "okienko36", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            
+            function WinOpen_szczepkowski_69_kod() {
+                window.open("zad_69_kod.html", "okienko37", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
+            function WinOpen_szczepkowski_69() {
+                window.open("zad_69.html", "okienko38", "toolbar=no,directories=no,menubar=no,fullscreen=yes");
+            }
